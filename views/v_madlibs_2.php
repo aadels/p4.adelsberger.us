cc225674e0348c6826e07a0164b7dfd671fe8c36
ann adelsberger <adelsbergerann@gmail.com>
2013-12-13 11:19:37 -0500

    edit madlib text

--- views/v_madlibs_2.php
+++ views/v_madlibs_2.php
@@ -10,7 +10,7 @@
 	<?=ucfirst($data['input3'])?> <?=ucfirst($data['input4'])?> picks up the <?=$data['input5']?>  in a <?=$data['input6']?> where a  <?=$data['input7']?> has been. <br>
 	<?=ucfirst($data['input8'])?> in a <?=$data['input9']?>! <br>
 
-	<?=ucfirst($data['input10'])?> at the <?=$data['input11']?>, wearing the <?=$data['input12']?> that she keeps in a(n) <?=$data['input13']?>  <?=$data['input14']?>. <br>Who is it for?<br><br>
+	<?=ucfirst($data['input10'])?> at the <?=$data['input11']?>, wearing the <?=$data['input12']?> that she keeps in a(n) <?=$data['input13']?> by the <?=$data['input14']?>. <br>Who is it for?<br><br>
 
 	All the <?=$data['input1']?> <?=$data['input2']?> where do they all come from?
 	All the <?=$data['input1']?> <?=$data['input2']?> where do they all belong?
