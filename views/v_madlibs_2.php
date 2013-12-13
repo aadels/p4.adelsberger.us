4adf3a111e17fa833188c352a8b49779b04540f4
ann adelsberger <adelsbergerann@gmail.com>
2013-12-13 09:06:39 -0500

    edit form 2

--- views/v_madlibs_2.php
+++ views/v_madlibs_2.php
@@ -7,7 +7,7 @@
 	Ah, look at all the <?=$data['input1']?>  <?=$data['input2']?>! <br>
 	Ah, look at all the <?=$data['input1']?>  <?=$data['input2']?>!  <br><br>
 
-	<?=ucfirst($data['input3'])?> <?=ucfirst($data['input4'])?> picks up the <?=$data['input5']?>in a <?=$data['input6']?> where a <?=$data['input7']?>has been. <br>
+	<?=ucfirst($data['input3'])?> <?=ucfirst($data['input4'])?> picks up the <?=$data['input5']?>  in a <?=$data['input6']?> where a  <?=$data['input7']?> has been. <br>
 	<?=ucfirst($data['input8'])?> in a <?=$data['input9']?>!" <br>
 
 	<?=ucfirst($data['input10'])?> at the <?=$data['input11']?>, wearing the <?=$data['input12']?> that she keeps in a(n) <?=$data['input13']?>  <?=$data['input14']?>. <br>Who is it for?<br><br>
